ce641a624211b1aba0224df568b2a63975e0042a
Vivek <gaikwadvivekshil@gmail.com>
2026-08-24 13:42:27 +0530

    Initial Commit

--- src/Patterns/PascalsTriangle.java
+++ src/Patterns/PascalsTriangle.java
@@ -8,6 +8,8 @@ public class PascalsTriangle {
         System.out.print("Enter the number: ");
         int num = input.nextInt();
 
+        System.out.println("I will do this letter...");
+
 
     }
 }
